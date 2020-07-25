0b5365b43469ec7721c72c196f93c9c826fba48d
maybecake <maybemaybecake@gmail.com>
2020-07-24 23:17:48 -0700

    testing position generation from finger parameters.

--- freecad/macros/finger.py
+++ freecad/macros/finger.py
@@ -1,27 +1,48 @@
 import attr
 import math
+from FreeCAD import Vector
 
 
 @attr.s
-class Finger(object):
+class Finger:
+    """A single finger."""
 
-    # Proximal phalanges length. Closest section to hand.
-    prox_mm = attr.ib(default=44)
-    mid_mm = attr.ib(default=23)   # Middle phalanges length. Middle part.
-    dist_mm = attr.ib(default=18)  # Distal phalanges length. Finger tip.
+    # TODO: Maybe add a flex angle to calcuate neighbor column.
+    # Lengths of the phalanges, ordered from closest from hand to furthest out.
+    segments_mm = attr.ib(default=[44, 23, 18])
 
-    p_deg = attr.ib(default=math.pi/2)     # Angle of proximal.
-    pm_deg = attr.ib(default=0)    # Angle between proximal and middle.
-    md_deg = attr.ib(default=0)    # Angle between middle and distal.
+    # TODO: maybe also allow for non-straight fingers?
+    # normal due to user reaching for key.
+    # Angle between segments, starting with intial angle of first segment.
+    rows = attr.ib(default=[
+        [90, 0, 0],
+        [90, 15, 15],
+        [90, 45, 45]])
 
-    def get_total(self):
-        return self.prox_mm + self.mid_mm + self.dist_mm
+    def __str__(self):
+        return '{}, {}'.format(self.segments_mm, self.angles_deg)
 
-    def get_pos(self):
-        x = math.cos(self.p_deg)*self.prox_mm + math.cos(self.pm_deg+self.p_deg) * \
-            self.mid_mm + math.cos(self.md_deg +
-                                   self.pm_deg+self.p_deg)*self.dist_mm
-        y = math.sin(self.p_deg)*self.prox_mm + math.sin(self.pm_deg+self.p_deg) * \
-            self.mid_mm + math.sin(self.md_deg +
-                                   self.pm_deg+self.p_deg)*self.dist_mm
-        return (x, y)
+    def get_pos(self, angles, offset=Vector()):
+        """Returns position coordinates for the tip of this finger"""
+        ang_sum = 0
+        v = Vector()
+        for seg, ang in zip(self.segments_mm, angles):
+            ang_sum += math.radians(ang)
+            v.y += math.cos(ang_sum)*seg
+            v.z += math.sin(ang_sum)*seg
+        return v.add(offset)
+
+    def get_normal(self):
+        """Returns the normal vector for a keypress from the current position."""
+        # TODO: implement.
+        # Normal should be calcuated for each key based on how the finger moves
+        # to activate the key.
+
+
+@ attr.s
+class Hand:
+    """A hand."""
+    fingers = attr.ib(default=[Finger(), Finger(), Finger()])
+
+    # Knuckle offsets from 0.
+    offsets = attr.ib(default=[Vector(), Vector(25, 10, 0), Vector(50, 0, 0)])
